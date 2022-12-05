18c372533f62dd87b745b3067ab077064a49144c
Vladimir Moiseev neo <lutii@ngs.ru>
2022-12-05 13:49:32 +0700

    models

--- MyShop/models.py
+++ MyShop/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Название категории')
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Название продукта')
+    text = models.TextField(verbose_name='Описание')
+    price = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Цена')
+    caategory = models.ForeignKey('Category', blank=True, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title}'
